a4f3b27559264c896be727bbff39965f54c68972
Trevor Russell <truss.42@gmail.com>
2018-05-17 02:24:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,16 @@
 var chocolateBars = ["snickers", "hundred grand", "kitakt", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  newArray = [element, ...array]
-  return(newArray)
+  array = [element, ...array]
+  return(array)
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return(array)
 }
 function addElementToEndOfArray(array, element) {
-  newArray = [...array, element]
-  return(newArray)
-}
+  array = [...array, element]
+  return(array
 function destructivelyAddElementToEndgOfArray(array, element) {
   array.push(element)
   return(array)
@@ -23,13 +22,12 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return(array.shift())
 }
 function removeElementFromBeginningOfArray(array) {
-  newArray = array.slice(1)
-  return(newArray)
-}
+  array = array.slice(1)
+  return(array)
 function destructivelyRemoveElementFromEndOfArray(array) {
   return(array.pop())
 }
 function removeElementFromEndOfArray(array) {
-  newArray = array.slice(0, array.length - 1)
-  return(newArray)
+  array = array.slice(0, array.length - 1)
+  return(array)
 }
